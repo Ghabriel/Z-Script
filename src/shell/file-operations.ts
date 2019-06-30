c4c784b131b9799a07d4bf332336b5a313e38d5d
Ghabriel <ghabriel.nunes@gmail.com>
2019-06-30 12:31:27 -0300

    Shell: added a renameFile() function

--- src/shell/file-operations.ts
+++ src/shell/file-operations.ts
@@ -1,6 +1,21 @@
 import * as fs from 'fs';
 
 /**
+ * Renames a file. Rejects if the operation fails.
+ */
+export function renameFile(oldPath: string, newPath: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        fs.rename(oldPath, newPath, err => {
+            if (err) {
+                return reject(err);
+            }
+
+            resolve();
+        });
+    });
+}
+
+/**
  * Deletes a file. Rejects if the operation fails.
  */
 export function deleteFile(filename: string): Promise<void> {
