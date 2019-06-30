37f18d689a93c9a6ef736846f70b91d8e0b5d7c4
Ghabriel <ghabriel.nunes@gmail.com>
2019-06-30 13:05:25 -0300

    Shell: added a createFolder() function

--- src/shell/file-operations.ts
+++ src/shell/file-operations.ts
@@ -1,6 +1,29 @@
 import * as fs from 'fs';
 
 /**
+ * Creates a folder at the given path with the given permissions (777 if not
+ * specified). Note that this is recursive, i.e parent folders are created if
+ * needed. If the target path already exists, rejection only happens if it's
+ * not a folder.
+ */
+export function createFolder(path: string, mode: string = '777'): Promise<void> {
+    return new Promise((resolve, reject) => {
+        const options: fs.MakeDirectoryOptions = {
+            mode: parseInt(mode, 8),
+            recursive: true,
+        };
+
+        fs.mkdir(path, options, err => {
+            if (err) {
+                return reject(err);
+            }
+
+            resolve();
+        });
+    });
+}
+
+/**
  * Renames a file or folder. Rejects if the operation fails. Note that if
  * `newPath` already exists and is a non-empty folder, the operation fails.
  */
