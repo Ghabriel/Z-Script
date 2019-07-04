22f7ca24d7556e414df722a1794c014fb5a5c346
Ghabriel <ghabriel.nunes@gmail.com>
2019-07-04 10:03:03 -0300

    file-stats is now sync/async except getRecursiveModificationTime

--- src/shell/core.ts
+++ src/shell/core.ts
@@ -20,6 +20,30 @@ export function discardValue<T>(value: Output<T>): Output<void> {
     }
 }
 
+export function consolidate<R, V1>(
+    values: [Output<V1>],
+    fn: (v1: V1) => R
+): Output<R>;
+
+export function consolidate<R, V1, V2>(
+    values: [Output<V1>, Output<V2>],
+    fn: (v1: V1, v2: V2) => R
+): Output<R>;
+
+export function consolidate<R>(
+    values: any[],
+    fn: (...args: any[]) => R
+): Output<R> {
+    if (values[0] instanceof Promise) {
+        return new Promise(async (resolve, reject) => {
+            const awaitedValues = await Promise.all(values);
+            resolve(fn(...awaitedValues));
+        });
+    }
+
+    return fn(...values);
+}
+
 // export function sequence<R1, R2>(
 //     f1: () => Output<R1>,
 //     f2: (value: R1) => Output<R2>
--- src/shell/file-stats.ts
+++ src/shell/file-stats.ts
@@ -1,58 +1,76 @@
 import * as fs from 'fs';
-import { execute } from './execute';
+import { Async, consolidate, Output, Sync } from './core';
 
-/**
- * Checks if `filenameA` was modified more recently than `filenameB`. Note that
- * this uses the recursive modification time, making it suitable for use in
- * lazy compilation (e.g checking if 'src' is newer than 'bin').
- */
-export async function isNewerThan(filenameA: string, filenameB: string): Promise<boolean> {
-    const timestampA = await getRecursiveModificationTime(filenameA);
-    const timestampB = await getRecursiveModificationTime(filenameB);
-    return timestampA > timestampB;
+abstract class BaseFileStats {
+    /**
+     * Checks if `filenameA` was modified more recently than `filenameB`. Note that
+     * this uses the recursive modification time, making it suitable for use in
+     * lazy compilation (e.g checking if 'src' is newer than 'bin').
+     */
+    isNewerThan(filenameA: string, filenameB: string): Output<boolean> {
+        const timestampA = this.getRecursiveModificationTime(filenameA);
+        const timestampB = this.getRecursiveModificationTime(filenameB);
+
+        return consolidate([timestampA, timestampB], (a, b) => a > b);
+    }
+
+    /**
+     * Returns a Unix Timestamp with the modification time of a file or folder.
+     * If the filename refers to a folder, the returned value takes into account
+     * the modification time of its contents. If that's not desired, use
+     * `getModificationTime()` instead.
+     */
+    getRecursiveModificationTime(filename: string): Output<number> {
+        // TODO
+        return 42;
+        // const output = await execute(
+        //     `find ${filename} -type f -exec stat {} --printf="%Y\\n" \\; | sort -n -r | head -n 1`
+        // );
+
+        // return parseInt(output.stdout);
+    }
+
+    /**
+     * Returns a Unix Timestamp with the modification time of a file or folder.
+     * Note that the modification time of a folder may be newer than a file it
+     * contains. To get the modification time of a folder _including its contents_,
+     * use `getRecursiveModificationTime()` instead.
+     */
+    getModificationTime(filename: string): Output<number> {
+        const stats = this.getFileStats(filename);
+        return consolidate([stats], s => Math.floor(s.mtimeMs / 1000));
+    }
+
+    /**
+     * Checks if the given path corresponds to a folder.
+     */
+    isFolder(path: string): Output<boolean> {
+        const stats = this.getFileStats(path);
+        return consolidate([stats], s => s.isDirectory());
+    }
+
+    protected abstract getFileStats(filename: string): Output<fs.Stats>;
 }
 
-/**
- * Returns a Unix Timestamp with the modification time of a file or folder.
- * If the filename refers to a folder, the returned value takes into account
- * the modification time of its contents. If that's not desired, use
- * `getModificationTime()` instead.
- */
-export async function getRecursiveModificationTime(filename: string): Promise<number> {
-    const output = await execute(
-        `find ${filename} -type f -exec stat {} --printf="%Y\\n" \\; | sort -n -r | head -n 1`
-    );
+class AsyncFileStatsImpl extends BaseFileStats {
+    protected getFileStats(filename: string): Promise<fs.Stats> {
+        return new Promise((resolve, reject) => {
+            fs.stat(filename, (err, stats) => {
+                if (err) {
+                    return reject(err);
+                }
 
-    return parseInt(output.stdout);
-}
-
-/**
- * Returns a Unix Timestamp with the modification time of a file or folder.
- * Note that the modification time of a folder may be newer than a file it
- * contains. To get the modification time of a folder _including its contents_,
- * use `getRecursiveModificationTime()` instead.
- */
-export async function getModificationTime(filename: string): Promise<number> {
-    const stats = await getFileStats(filename);
-    return Math.floor(stats.mtimeMs / 1000);
-}
-
-/**
- * Checks if the given path corresponds to a folder.
- */
-export async function isFolder(path: string): Promise<boolean> {
-    const stats = await getFileStats(path);
-    return stats.isDirectory();
-}
-
-function getFileStats(filename: string): Promise<fs.Stats> {
-    return new Promise((resolve, reject) => {
-        fs.stat(filename, (err, stats) => {
-            if (err) {
-                return reject(err);
-            }
-
-            resolve(stats);
+                resolve(stats);
+            });
         });
-    });
+    }
 }
+
+class SyncFileStatsImpl extends BaseFileStats {
+    protected getFileStats(filename: string): fs.Stats {
+        return fs.statSync(filename);
+    }
+}
+
+export const AsyncFileStats = new AsyncFileStatsImpl() as Async<BaseFileStats>;
+export const SyncFileStats = new SyncFileStatsImpl() as Sync<BaseFileStats>;
