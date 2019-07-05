745578107f3990beaee17f4e65a4b8a19bec8292
Ghabriel <ghabriel.nunes@gmail.com>
2019-07-05 09:45:05 -0300

    Documented addCommand, runCommand and run functions

--- src/core/core-functions.ts
+++ src/core/core-functions.ts
@@ -3,14 +3,26 @@ import { run as externalRun, runCommand as externalRunCommand } from './run';
 
 const commandList: Command[] = [];
 
+/**
+ * Adds a command to the script. The given function will receive all arguments
+ * passed after it.
+ */
 export function addCommand(name: string, execute: ExecutionFunction): void {
     commandList.push({ name, execute });
 }
 
+/**
+ * Runs a registered command with a given list of arguments.
+ */
 export function runCommand(name: string, args: string[] = []): void {
     externalRunCommand(commandList, name, args);
 }
 
+/**
+ * Runs the registered command that matches the input arguments ("all" if no
+ * command was provided). This function should be called at the end of every
+ * Z-Script.
+ */
 export function run(): void {
     externalRun(commandList);
 }
