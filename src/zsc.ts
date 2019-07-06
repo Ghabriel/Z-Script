6256344039b67eeb0f86c73add05eaf2c039f3c6
Ghabriel <ghabriel.nunes@gmail.com>
2019-07-05 22:40:26 -0300

    Refactored zsc.ts

--- src/zsc.ts
+++ src/zsc.ts
@@ -4,9 +4,18 @@ const ZSCRIPT_FILE = 'zscript.ts';
 const ZSCRIPT_JS_FILE = 'zscript.js';
 const ZSCRIPT_CACHE_FILE = '.zscript_cache.js';
 
+const FIX_FILENAME = `mv ${ZSCRIPT_JS_FILE} ${ZSCRIPT_CACHE_FILE}`;
+
 main();
 
 function main() {
+    abortIfNoZScript();
+    recompileIfNeeded();
+    const args = process.argv.slice(2);
+    runZScript(args);
+}
+
+function abortIfNoZScript() {
     if (!Shell.fileExists(ZSCRIPT_FILE)) {
         Format.foreground.set(Color.Red);
         Format.bold.set();
@@ -14,18 +23,16 @@ function main() {
         Format.reset();
         console.log(`no ${ZSCRIPT_FILE} file found.`);
         process.exit(1);
-        return;
     }
+}
 
+function recompileIfNeeded() {
     if (needsRecompilation()) {
         Format.foreground.set(Color.Yellow);
         console.log('Compiling Z-Script...');
         Format.reset();
-        exec(`npx tsc ${ZSCRIPT_FILE} && mv ${ZSCRIPT_JS_FILE} ${ZSCRIPT_CACHE_FILE}`);
+        exec(`npx tsc ${ZSCRIPT_FILE} && ${FIX_FILENAME}`);
     }
-
-    const args = process.argv.slice(2);
-    exec(`node ${ZSCRIPT_CACHE_FILE} ${args.join(' ')}`);
 }
 
 function needsRecompilation(): boolean {
@@ -34,4 +41,8 @@ function needsRecompilation(): boolean {
     }
 
     return Shell.isNewerThan(ZSCRIPT_FILE, ZSCRIPT_CACHE_FILE);
+}
+
+function runZScript(args: string[]) {
+    exec(`node ${ZSCRIPT_CACHE_FILE} ${args.join(' ')}`);
 }
