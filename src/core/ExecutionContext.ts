c17899e373ef43ee0b4e7e4311df5b0de4294573
Ghabriel <ghabriel.nunes@gmail.com>
2019-07-07 12:11:30 -0300

    Documented ExecutionContext

--- src/core/ExecutionContext.ts
+++ src/core/ExecutionContext.ts
@@ -3,15 +3,25 @@ import { Command, ExecutionFunction } from './Command';
 import { DEFAULT_RULE_NAME } from './constants';
 import { NoSuchRuleError } from './NoSuchRuleError';
 
+/**
+ * Represents an isolated context in which commands can be registered and ran.
+ */
 export class ExecutionContext {
     private commandList: Command[] = [];
 
     constructor(private args: string[] = []) { }
 
+    /**
+     * Adds a command to this context. The given function, when called, will
+     * receive all arguments passed after it.
+     */
     addCommand(name: string, execute: ExecutionFunction) {
         this.commandList.push({ name, execute });
     }
 
+    /**
+     * Runs a registered command with a given list of arguments.
+     */
     runCommand(name: string, args: string[] = []) {
         try {
             this.internalRunCommand(name, args);
@@ -25,6 +35,10 @@ export class ExecutionContext {
         }
     }
 
+    /**
+     * Runs the registered command that matches the input arguments ("main" if
+     * no command was provided).
+     */
     run() {
         const [commandName, ...args] = this.args;
         this.runCommand(commandName || DEFAULT_RULE_NAME, args);
